e49e8d3668e12e687dfd816b085da001a2950676
Muhammad Khan <130873824+umairkhan2582@users.noreply.github.com>
2025-11-16 06:35:18 +0500

    Add client/src/hooks/useWallet.tsx

---
+++ client/src/hooks/useWallet.tsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { WalletContext } from '@/contexts/WalletContext';
+
+export const useWallet = () => {
+  const context = useContext(WalletContext);
+  
+  if (context === undefined) {
+    throw new Error('useWallet must be used within a WalletProvider');
+  }
+  
+  return context;
+};
