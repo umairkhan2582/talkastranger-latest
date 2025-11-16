3685f129567dbeb3b8e51416f64c3647a9ba72d6
Muhammad Khan <130873824+umairkhan2582@users.noreply.github.com>
2025-11-16 06:34:34 +0500

    Add client/src/types/window.d.ts

---
+++ client/src/types/window.d.ts
@@ -0,0 +1,13 @@
+interface Window {
+  ethereum?: {
+    isMetaMask?: boolean;
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on: (event: string, callback: (...args: any[]) => void) => void;
+    removeListener: (event: string, callback: (...args: any[]) => void) => void;
+    selectedAddress?: string;
+    chainId?: string;
+    networkVersion?: string;
+    isConnected: () => boolean;
+    enable: () => Promise<string[]>;
+  };
+}
